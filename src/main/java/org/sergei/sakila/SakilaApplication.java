1c9c211fca1c98a4eec3525b735d59e51c0e267f
Sergei <sergei.visotsky@gmail.com>
2019-02-08 22:08:28 +0200

    Profile check in main class added

--- src/main/java/org/sergei/sakila/SakilaApplication.java
+++ src/main/java/org/sergei/sakila/SakilaApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SakilaApplication {
 
     public static void main(String[] args) {
+
+        if (System.getProperty("-Dspring.profiles.active") == null) {
+            System.setProperty("-Dspring.profiles.active", "dev");
+        }
+
         SpringApplication.run(SakilaApplication.class, args);
     }
 
