0faa69d37331cd6bd496790a3ea6bb6311e8ef37
Sergei <sergei.visotsky@gmail.com>
2019-02-09 12:56:41 +0200

    Beans are shown, logback modified

--- src/main/java/org/sergei/sakila/SakilaApplication.java
+++ src/main/java/org/sergei/sakila/SakilaApplication.java
@@ -1,19 +1,31 @@
 package org.sergei.sakila;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SakilaApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SakilaApplication.class);
+
+    public static void getBeans(ApplicationContext applicationContext) {
+        String[] beans = applicationContext.getBeanDefinitionNames();
+        for (String bean : beans) {
+            LOGGER.debug("Bean: {}", applicationContext.getBean(bean));
+        }
+    }
+
     public static void main(String[] args) {
 
         if (System.getProperty("-Dspring.profiles.active") == null) {
-            System.setProperty("-Dspring.profiles.active", "dev");
+            System.setProperty("-Dspring.profiles.active", "prod");
         }
 
-        SpringApplication.run(SakilaApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(SakilaApplication.class, args);
+
+        getBeans(applicationContext);
     }
-
 }
-
